cbc0e868468443a57806e96e10c1d2570587e657
codonward <patrick@engineerapart.com>
2018-02-03 12:28:52 -0600

    Update for cordova android 7

--- src/android/update_manifest.js
+++ src/android/update_manifest.js
@@ -1,56 +1,63 @@
 #!/usr/bin/env node
 
 var fs = require('fs'),
-    path = require('path'),
-    util = require('util');
-var inspect = function (obj) { return util.inspect(obj, false, Infinity, false); }
+  path = require('path');
 
 module.exports = function (context) {
-    console.log(inspect(context));
-    var deferral = context.requireCordovaModule('q').defer();
+  const androidPlatform = context.cordova.platforms.android;
+  const versionInt = parseInt(androidPlatform.version.replace(/[^0-9\.]/g, ''));
+  console.log('[cordova-plugin-inappbrowser-popup-bridge]==> Found cordova-android ', versionInt);
 
-    var toolsAttribute = "xmlns:tools=\"http://schemas.android.com/tools\"";
-    var manifestOpen = "<manifest";
+  var deferral = context.requireCordovaModule('q').defer();
 
-    var overrideAttribute = "tools:overrideLibrary=\"com.braintreepayments.popupbridge\"";
-    var usesSdkOpen = "<uses-sdk";
+  var toolsAttribute = "xmlns:tools=\"http://schemas.android.com/tools\"";
+  var manifestOpen = "<manifest";
 
-    var projectRoot = context.opts.projectRoot;
-    var platformRoot = path.join(projectRoot, 'platforms/android');
-    var manifestPath = path.join(platformRoot, 'AndroidManifest.xml');
-    console.log("manifestPath: " + manifestPath);
+  var overrideAttribute = "tools:overrideLibrary=\"com.braintreepayments.popupbridge\"";
+  var usesSdkOpen = "<uses-sdk";
 
-    fs.readFile(manifestPath, function(err, manifest) {
-        if(err || !manifest){
-            deferral.reject("Failed to read AndroidManifest.xml: " + err);
+  var projectRoot = context.opts.projectRoot;
+  var platformRoot = path.join(projectRoot, 'platforms/android');
+  var manifestPath = path.join(platformRoot, 'AndroidManifest.xml');
+
+  if (versionInt >= 7) {
+    // Correspondingly, config.xml would be at app/src/main/res/xml/config.xml
+    manifestPath = path.join(platformRoot, 'app', 'src', 'main', 'AndroidManifest.xml');
+  }
+  console.log("manifestPath: " + manifestPath);
+
+  fs.readFile(manifestPath, function (err, manifest) {
+    if (err || !manifest) {
+      deferral.reject("[cordova-plugin-inappbrowser-popup-bridge]==>Failed to read AndroidManifest.xml: " + err);
+      return;
+    }
+
+    manifest = manifest.toString();
+
+    var shouldWrite = false;
+    if (manifest.indexOf(overrideAttribute) == -1) {
+      manifest = manifest.replace(usesSdkOpen, usesSdkOpen + " " + overrideAttribute + " ");
+      shouldWrite = true;
+    }
+
+    if (manifest.indexOf(toolsAttribute) == -1) {
+      manifest = manifest.replace(manifestOpen, manifestOpen + " " + toolsAttribute + " ");
+      shouldWrite = true;
+    }
+
+    if (shouldWrite) {
+      fs.writeFile(manifestPath, manifest, 'utf8', function (err) {
+        if (err) {
+          deferral.reject("Failed to write AndroidManifest.xml: " + err);
         }
+        // console.log("wrote");
+        deferral.resolve();
+      });
+    } else {
+      // console.log("no write required");
+      deferral.resolve();
+    }
 
-        manifest = manifest.toString();
-
-        var shouldWrite = false;
-        if(manifest.indexOf(overrideAttribute) == -1) {
-            manifest = manifest.replace(usesSdkOpen, usesSdkOpen + " " + overrideAttribute + " ");
-            shouldWrite = true;
-        }
-
-        if(manifest.indexOf(toolsAttribute) == -1) {
-            manifest = manifest.replace(manifestOpen, manifestOpen + " " + toolsAttribute + " ");
-            shouldWrite = true;
-        }
-
-        if(shouldWrite){
-            fs.writeFile(manifestPath, manifest, 'utf8', function (err) {
-                if (err) {
-                    deferral.reject("Failed to write AndroidManifest.xml: " + err);
-                }
-                console.log("wrote");
-                deferral.resolve();
-            });
-        }else{
-            console.log("no write required");
-            deferral.resolve();
-        }
-
-    });
-    return deferral.promise;
+  });
+  return deferral.promise;
 };
